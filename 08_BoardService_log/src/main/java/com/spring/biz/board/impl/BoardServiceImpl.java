06a6a8c4efbaa23529d6fe5cd23aeffb1cdb592a
dusjh <79893756+dusjh@users.noreply.github.com>
2021-06-26 23:34:36 +0900

    BoardService_log
    BoardService_log

---
+++ 08_BoardService_log/src/main/java/com/spring/biz/board/impl/BoardServiceImpl.java
@@ -0,0 +1,57 @@
+package com.spring.biz.board.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.LocalDataSourceJobStore;
+import org.springframework.stereotype.Service;
+
+import com.spring.biz.board.BoardService;
+import com.spring.biz.board.BoardVO;
+import com.spring.biz.common.Log4jAdvice;
+import com.spring.biz.common.LogAdvice;
+
+// @Service: @Component 상속 확장 어노테이션
+//		-비즈니스 로직 처리 서비스 영역에 사용
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+	
+	@Autowired  //타입이 일치하는 객체(인스턴스) 주입
+	private BoardDAO boardDAO;
+	//private LogAdvice log;
+	private Log4jAdvice log4j;
+	
+	public BoardServiceImpl() {
+		System.out.println(">> BoardServiceImpl() 객체 생성");
+		//log = new LogAdvice();
+		log4j = new Log4jAdvice();
+	}
+
+	@Override
+	public void insertBoard(BoardVO vo) {
+		log4j.printLogging();
+		boardDAO.insertBoard(vo);
+	}
+
+	@Override
+	public void updateBoard(BoardVO vo) {
+		boardDAO.updateBoard(vo);
+	}
+
+	@Override
+	public void deleteBoard(BoardVO vo) {
+		boardDAO.deleteBoard(vo);
+	}
+
+	@Override
+	public BoardVO getBoard(BoardVO vo) {
+		return boardDAO.getBoard(vo);
+	}
+
+	@Override
+	public List<BoardVO> getBoardList() {
+		return boardDAO.getBoardList();
+		
+	}
+
+}
