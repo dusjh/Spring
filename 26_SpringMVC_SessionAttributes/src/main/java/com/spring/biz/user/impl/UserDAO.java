5f4807c32168331e0f04ab1d24b9ca5c467062dd
dusjh <79893756+dusjh@users.noreply.github.com>
2021-07-08 19:02:14 +0900

    SpringMVC_SessionAttributes
    SpringMVC_SessionAttributes

---
+++ 26_SpringMVC_SessionAttributes/src/main/java/com/spring/biz/user/impl/UserDAO.java
@@ -0,0 +1,52 @@
+package com.spring.biz.user.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.springframework.stereotype.Repository;
+
+import com.spring.biz.common.JDBCUtil;
+import com.spring.biz.user.UserVO;
+
+//DB연결 어노테이션
+@Repository("userDAO")
+public class UserDAO {
+	//JDBC 관련 변수(필드)-JDBC 연결
+		private Connection conn;
+		private PreparedStatement stmt;
+		private ResultSet rs;
+		
+		public UserDAO() {
+			System.out.println(">> UserDAO() 객체 생성");
+		}
+		
+	//Sql문
+	private final String USER_GET
+		= "SELECT * FROM USERS WHERE ID = ? AND PASSWORD = ?";
+	
+	public UserVO getUser(UserVO vo) {
+		System.out.println("---> JDBC로 getUser() 실행");
+		UserVO user = null;
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(USER_GET);
+			stmt.setString(1, vo.getId());
+			stmt.setString(2, vo.getPassword());
+			
+			rs = stmt.executeQuery(); //select 문장에서는 -> executeQuery: 질문을 던져 결과를 받음
+			if(rs.next()) {
+				user = new UserVO(rs.getString("ID"), 
+									rs.getString("PASSWORD"), 
+									rs.getString("NAME"), 
+									rs.getString("ROLE"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(conn, stmt, rs);
+		}
+		return user;
+	}
+	
+}
---
+++ 26_SpringMVC_SessionAttributes/src/main/java/com/spring/biz/view/board/BoardController.java
@@ -0,0 +1,90 @@
+package com.spring.biz.view.board;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.spring.biz.board.BoardVO;
+import com.spring.biz.board.impl.BoardDAO;
+
+@Controller
+@SessionAttributes("board") //board라는 이름의 Model이 있으면 session에 저장
+//Board관련 controller
+public class BoardController {
+
+	//메서드에 선언된 @ModelAttribute는 리턴된 데이터를 View에 전달
+	//@ModelAttribute 선언된 메서드는 @RequestMapping 메서드 보다 먼저 실행
+	//View에 전달될 때 설정된 명칭 ("conditionMap")으로 전달된다.
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+		System.out.println(">> @ModelAttribute - Map searchConditionMap() 실행");
+		//key: 제목, value: TITLE
+		//key: 내용, value: CONTENT
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		
+		return conditionMap;
+	}
+	//리턴타입 ModelAndView -> String으로 변경
+	//전달할 데이터 저장했던 ModelAndView -> Model 타입으로 변경
+	
+	//상세화면
+	@RequestMapping("/getBoard.do")
+	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
+		System.out.println(">> 게시글 상세보기");
+		BoardVO board = boardDAO.getBoard(vo);
+		System.out.println("getBoard DB board: "+board);
+
+		model.addAttribute("board", board); //Model 객체 사용하여 View로 데이터 전달
+		return "getBoard.jsp";
+	}
+	
+	//select
+	@RequestMapping("/getBoardList.do")
+	public String getBoardList(BoardVO vo,	BoardDAO boardDAO, Model model) {
+		System.out.println(">>> 게시글 전체 목록 보여주기");
+		System.out.println("vo: "+vo);
+
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);		
+		model.addAttribute("boardList",boardList);
+		return "getBoardList.jsp";
+	}
+	
+	//insert
+	@RequestMapping("/insertBoard.do")
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
+		System.out.println(">>> 게시글 입력");
+		System.out.println("vo: "+vo);
+		boardDAO.insertBoard(vo);
+		
+		return "getBoardList.do";
+	}
+	
+	//update
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+		System.out.println(">>> 게시글 수정");
+		System.out.println("update vo: "+vo);
+		boardDAO.updateBoard(vo);
+		
+		return "getBoardList.do";
+	}
+	
+	//delete
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+		System.out.println(">>> 게시글 삭제");
+		System.out.println("vo: "+vo);
+		boardDAO.deleteBoard(vo);
+		
+		return "getBoardList.do";
+	}
+}
